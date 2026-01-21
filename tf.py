625dff31baefacadfa66e16ef669616b14a73981
Jarthianur <jarthianur.github@gmail.com>
2026-01-21 11:02:03 +0100

    let format default to {}

--- tf.py
+++ tf.py
@@ -3,15 +3,17 @@ import sys
 import re
 import os.path
 
-if len(sys.argv) != 3:
+if len(sys.argv) < 2:
     print("Usage: {} <regex> <format>".format(os.path.basename(sys.argv[0])))
     exit(0)
 try:
     r = re.compile(sys.argv[1])
 except re.error as e:
-    print( "ERROR: {}".format(e), file=sys.stderr)
+    print("ERROR: {}".format(e), file=sys.stderr)
     exit(1)
-f = sys.argv[2]
+f = "{}"
+if len(sys.argv) > 2:
+    f = sys.argv[2]
 for line in sys.stdin:
     m = r.search(line)
     if m is None:
